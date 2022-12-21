1df4d876d6342c29dc96c015f820045c28cacf70
jan <>
2022-12-21 18:36:54 +0100

    Zwischenstand

--- game.py
+++ game.py
@@ -1,38 +1,40 @@
-class gameObj:
-    def __init__(self,obj,x,y):
-        self.shape = obj
-        self.x = x
-        self.y = y
-        self.visible = True
+# class gameObj:
+    # def __init__(self,obj,x,y):
+        # self.shape = obj
+        # self.x = x
+        # self.y = y
+        # self.visible = True
     
-    def setVisiblility(self,b):
-        self.visible = b
+    # def setVisiblility(self,b):
+        # self.visible = b
 
-    def toggleVisible(self):
-        self.visible = not self.visible 
+    # def toggleVisible(self):
+        # self.visible = not self.visible 
 
-    def isVisible(self):
-        return self.visible
+    # def isVisible(self):
+        # return self.visible
 
-    def pos(self):
-        return (self.x,self.y)
+    # def pos(self):
+        # return (self.x,self.y)
 
-    def moveBy(self,dx,dy):
-        self.x += dx
-        self.y += dy
+    # def moveBy(self,dx,dy):
+        # self.x += dx
+        # self.y += dy
 
-    def moveTo(self,x,y):
-        self.x = x
-        self.y = y
+    # def moveTo(self,x,y):
+        # self.x = x
+        # self.y = y
 
-class pixel:
-    def __init__(self,rgb):
-        self.triple = rgb
+# class pixel:
+    # def __init__(self,rgb):
+        # self.triple = [r,g,b]
 
-class shape:
-    def __init__(self,w,h):
-        self.w = w
-        self.h = h
-        self.pixels = [
+# class Shape:
+    # def __init__(self,w,h):
+        # self.w = w
+        # self.h = h
+        # self.pixels = [
 
+def readGameObj(path):
+    f = open(path).read
 
--- lighthouse_demo.py
+++ lighthouse_demo.py
@@ -8,30 +8,11 @@ Pyghthouse.start(p)
 
 img = Pyghthouse.empty_image()
 
-class gameObj:
-    __init__(obj,x,y):
-        self.shape = obj
-        self.x = x
-        self.y = y
-        self.visible = True
-    
-    def setVisible(b):
-        self.visible = b
-
-    def isVisible():
-        return self.visible
-
-    def pos():
-        return (x,y)
-
-    def move(dx,dy):
-        self.x += dx
-        self.y += dy
 
 
 while True:
     for x in range(0, 28):
         for y in range(0, 14):
             img[y][x] = [randint(0, 255), randint(0, 255), randint(0,255)]
-    sleep(0.5)
+    sleep(0.10)
     Pyghthouse.set_image(p, img)
