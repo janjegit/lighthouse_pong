1a86cbfc6e578f7e93ffbc1c266fabc2037e2524
Jan <gitjaheje@mailbox.org>
2022-12-22 18:25:30 +0100

    Demo reinkopiert

---
+++ lighthouse_demo.py
@@ -0,0 +1,16 @@
+from pyghthouse import Pyghthouse #Muss ggf. installiert werden!
+from login import username, token #Nicht vergessen, dort die Daten anzupassen!
+from random import randint
+from time import sleep
+
+p = Pyghthouse(username, token)
+Pyghthouse.start(p)
+
+img = Pyghthouse.empty_image()
+
+while True:
+    for x in range(0, 28):
+        for y in range(0, 14):
+            img[y][x] = [randint(0, 255), randint(0, 255), randint(0,255)]
+    sleep(0.5)
+    Pyghthouse.set_image(p, img)
