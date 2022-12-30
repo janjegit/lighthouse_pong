3749907d5187e832b69f01a6ef5642f1806d3b06
Jan <gitjaheje@mailbox.org>
2022-12-30 18:53:41 +0100

    Zwischenstand

--- game.py
+++ game.py
@@ -1,9 +1,97 @@
+import random
+
 from pyghthouse import Pyghthouse, VerbosityLevel
 from login import username, token
 from time import sleep
 
 from vector import Vector2D
 from gameobj import GameObj, Color
+
+class Paddle(GameObj):
+
+    def update(self,f=Vector2D(0,0)):
+        self.applyForce(f)
+        self.acclerate()
+        self.clearAcc()
+        self.move()
+
+        displ_h=14
+        displ_w=28
+        boundry = False
+        if self.pos.y + self.h > displ_h:
+            x,y = self.pos.x, (displ_h - self.h)
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(1,-1)
+            boundry = True
+
+        if self.pos.y < 0:
+            x,y = self.pos.x, 0.0
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(1,-1)
+            boundry = True
+
+        if self.pos.x + self.w > displ_w:
+            x,y = (displ_w - self.w), self.pos.y
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(-1,1)
+            boundry = True
+
+        if self.pos.x < 0:
+            x,y = 0.0 , self.pos.y
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(-1,1)
+            boundry = True
+
+        if boundry:
+            r = random.randint(100,255)
+            g = random.randint(100,255)
+            b = random.randint(100,255)
+            self.color = Color(r,g,b)
+
+# Ball
+class Ball(GameObj):
+
+
+    def update(self,f=Vector2D(0,0)):
+        self.applyForce(f)
+        self.acclerate()
+        self.clearAcc()
+        self.move()
+
+        displ_h=14
+        displ_w=28
+        boundry = False
+        if self.pos.y + self.h > displ_h:
+            x,y = self.pos.x, (displ_h - self.h)
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(1,-1)
+            boundry = True
+
+        if self.pos.y < 0:
+            x,y = self.pos.x, 0.0
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(1,-1)
+            boundry = True
+
+        if self.pos.x + self.w > displ_w:
+            x,y = (displ_w - self.w), self.pos.y
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(-1,1)
+            boundry = True
+
+        if self.pos.x < 0:
+            x,y = 0.0 , self.pos.y
+            self.moveTo(x,y)
+            self.vel = self.vel * Vector2D(-1,1)
+            boundry = True
+
+        if boundry:
+            r = random.randint(100,255)
+            g = random.randint(100,255)
+            b = random.randint(100,255)
+            self.color = Color(r,g,b)
+
+
 
 def draw(p,gameObjs):
     img = p.empty_image()
@@ -14,19 +102,24 @@ def draw(p,gameObjs):
                 x = int(x)
                 y = int(y)
                 if y>=0 and y<len(img) and x>=0 and x<len(img[0]):
-                    img[y][x] = [255, 255, 255]
+                    c = obj.color
+                    img[y][x] = [c.r, c.g, c.b]
     p.set_image(img)
 
 def update(objs):
-    for _,obj in objs.items():
+    for name,obj in objs.items():
         obj.update()
 
 def initGameObj():
     displDim = (28,14) 
-    BALL_START = (displDim[0]//2,0)
+    BALL_START = (displDim[0]//2,displDim[1]//2)
+    PL1_START = (displDim[0]//2,1)
+    PL2_START = (displDim[0]//2,displDim[1]-2)
     
-    objects = {"ball" : GameObj("ball",1,1,BALL_START)}
-    objects["ball"].vel = Vector2D(1,1)
+    objects = {"ball" : Ball("ball",2,1,BALL_START,Color(255,0,0)),
+               "pl1" : GameObj("pl1",5,1,PL1_START,Color(255,255,255)),
+               "pl2" : GameObj("pl2",5,1,PL2_START,Color(255,255,255))}
+    objects["ball"].vel = Vector2D(0.5,0.5)
     
     return objects
 
--- gameobj.py
+++ gameobj.py
@@ -14,6 +14,7 @@ class GameObj:
         self.pos = Vector2D.fromTuple(pos) 
         self.acc = Vector2D()
         self.vel = Vector2D()
+        self.color = color
         self.pixels = [(i % w, i // w) for i in range(w*h)]
         self.visible = True
     
@@ -36,33 +37,7 @@ class GameObj:
         self.acc = self.acc + f
     
     def update(self):
-        self.applyForce(Vector2D(0.0,0.7))
-        self.acclerate()
-        self.clearAcc()
-        self.move()
-
-        displ_h=14
-        displ_w=28
-
-        if self.pos.y + self.h > displ_h:
-            x,y = self.pos.x, (displ_h - self.h)
-            self.moveTo(x,y)
-            self.vel = self.vel * Vector2D(0.8,-0.8)
-
-        if self.pos.y < 0:
-            x,y = self.pos.x, 0.0
-            self.moveTo(x,y)
-            self.vel = self.vel * Vector2D(0.8,-0.8)
-
-        if self.pos.x + self.w > displ_w:
-            x,y = (displ_w - self.w), self.pos.y
-            self.moveTo(x,y)
-            self.vel = self.vel * Vector2D(-0.8,0.8)
-
-        if self.pos.x < 0:
-            x,y = 0.0 , self.pos.y
-            self.moveTo(x,y)
-            self.vel = self.vel * Vector2D(-0.8,0.8)
+        pass
         
     def getPixels(self):
         x,y = self.pos.x,self.pos.y
